2cff86d01128a664aed2b3ab3d7e8ff0e1df8162
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-02-13 00:13:53 +0600

    last

--- js/script.js
+++ js/script.js
@@ -1,22 +1,3 @@
-// let box = document.getElementsByClassName('box')[0];
-// let col = document.getElementsByClassName('col-2')[0];
-// // let active = document.getElementsByClassName('active');
-// box.addEventListener("click", function(event){
-//         let next = col.nextElementSibling.classList.add('active');
-//         if (box.classList.contains('active')){
-//         box.classList.remove('active');
-//         console.log("example");
-//         }
-// console.log(event)
-//            // let toRemove = document.querySelector('active');
-// 	// toRemove.parentNode.removeChild(toRemove);
-// })
-// let products = document.getElementsByClassName('row box');
-// let rowWrapper = document.getElementById('rowWrapper');
-// rowWrapper.addEventListener('click', function(event){
-//     let current = products.index(this)
-//     console.log(current)
-// })
 let rowWrapper = document.getElementById('rowWrapper');
 let i;
 let active = document.getElementsByClassName('active')
@@ -26,12 +7,6 @@ rowWrapper.addEventListener('click', function(event) {
         startPoint.classList.remove('active')
     }
   let index = [...rowWrapper.children].findIndex(el => el == event.target)
-  let box = document.getElementsByClassName('box')[index];
+  let box = document.getElementsByClassName('box')[index +3];
   let next = box.nextElementSibling.classList.add('active');
-// while (box.classList.contains('active')) {
-//     box.classList.remove('active')
-// }
-//   if (box.classList.contains('active')){
-//     box.classList.remove('active')
-//     }
 })
