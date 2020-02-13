ff49a23f033072ba9cf9d50bc0d8e6aac883af3e
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-02-13 23:35:44 +0600

    end

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
-let rowWrapper = document.getElementById('rowWrapper');
-let i;
-let active = document.getElementsByClassName('active')
+// let rowWrapper = document.getElementById('row');
+// let i;
+// let products = document.getElementsByClassName('products')
+// document.addEventListener('click', function(event) {
+//   let index = [...rowWrapper.children].findIndex(el => el == event.target)
+//     let box = document.getElementsByClassName('products')[index+2].target;
+//   console.log(box);
+//   let next = box.nextElementSibling.location.hash;
+//   console.log(next)
+// })
+// let count = 0;
+//   let balloon = document.getElementsByClassName("balloon__childs")
+//   balloon.addEventListener("click", function(event){
+//        document.getElementsByClassName('balloon__count_item')[count].src = event.target.src
+//        count++
+//       console.log(event.target.src)
+//   })
+//   let small = document.getElementsByClassName('small-img');
+// document.addEventListener('click',function(event){
+//    if(event.target.classList.contains('small-img')){
+//       document.querySelector('.big-img').src = event.target.src
+//    }
+// })
+let rowWrapper = document.getElementById('row');
+let products = document.getElementsByClassName('GridProducts')
+console.log(products)
 rowWrapper.addEventListener('click', function(event) {
-    for (i=0;i<[...rowWrapper.children].length;i++) {
-        let startPoint = document.querySelectorAll('.box')[i];
-        startPoint.classList.remove('active')
-    }
-  let index = [...rowWrapper.children].findIndex(el => el == event.target)
-  let box = document.getElementsByClassName('box')[index +2];
-  let next = box.nextElementSibling.classList.add('active');
+  let index = [...rowWrapper.children].findIndex(el => el == event.target.parentNode)
+  if(event.target.parentNode.classList.contains('GridProducts')){
+    console.log(document.getElementsByClassName('products')[index+3].href);;
+    window.location.href = document.getElementsByClassName('products')[index+3].href;
+    // document.getElementsByClassName('products')[index].href = document.getElementsByClassName('products')[index+2].href;
+    // let box = document.getElementsByClassName('products')[index+2].href;)
+ }
+//  console.log(document.getElementsByClassName('products')[index+2].href);
 })
