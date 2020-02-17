31905008d83f973997a89f2012051afb47f69dd5
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-02-18 00:09:33 +0600

    gh-pages

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-let resultRightBox = document.getElementsByClassName('ResultsRigthBoxWrapper')[0];
-resultRightBox.addEventListener('click', function(event) {
+// let resultRightBox = document.getElementsByClassName('ResultsRigthBoxWrapper')[0];
+// let array = [];
+// console.log(resultRightBox)
+// resultRightBox.addEventListener('click', function(event) {
+//   'use strict';
+//   event.preventDefault();
+//   let products = document.getElementsByClassName('productListThumb');
+//   let index = [...products].findIndex(el => el === event.target);
+//   if (event.target.classList.contains('productListThumb')) {
+//     window.location.href = document.getElementsByClassName('productListThumb')[index+3].href;
+//     console.log(window.location.href = document.getElementsByClassName('productListThumb')[index+3].href)
+//   }
+// });
+document.querySelector('.tResults').addEventListener('click', function(event) {
   'use strict';
   event.preventDefault();
   let products = document.getElementsByClassName('productListThumb');
   let index = [...products].findIndex(el => el === event.target);
-  if (event.target.classList.contains('productListThumb')) {
+  if (event.target.parentNode.classList.contains('prodImageInner')) {
     window.location.href = document.getElementsByClassName('productListThumb')[index+3].href;
-    console.log(window.location.href = document.getElementsByClassName('productListThumb')[index+3].href)
   }
 });
