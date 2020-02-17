e1f2320de87f3581a66a66e47093113206b8e2d4
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-02-17 21:18:00 +0600

    end

--- js/script.js
+++ js/script.js
@@ -1,36 +1,9 @@
-// let rowWrapper = document.getElementById('row');
-// let i;
-// let products = document.getElementsByClassName('products')
-// document.addEventListener('click', function(event) {
-//   let index = [...rowWrapper.children].findIndex(el => el == event.target)
-//     let box = document.getElementsByClassName('products')[index+2].target;
-//   console.log(box);
-//   let next = box.nextElementSibling.location.hash;
-//   console.log(next)
-// })
-// let count = 0;
-//   let balloon = document.getElementsByClassName("balloon__childs")
-//   balloon.addEventListener("click", function(event){
-//        document.getElementsByClassName('balloon__count_item')[count].src = event.target.src
-//        count++
-//       console.log(event.target.src)
-//   })
-//   let small = document.getElementsByClassName('small-img');
-// document.addEventListener('click',function(event){
-//    if(event.target.classList.contains('small-img')){
-//       document.querySelector('.big-img').src = event.target.src
-//    }
-// })
-let rowWrapper = document.getElementById('row');
-let products = document.getElementsByClassName('GridProducts')
-console.log(products)
-rowWrapper.addEventListener('click', function(event) {
-  let index = [...rowWrapper.children].findIndex(el => el == event.target.parentNode)
-  if(event.target.parentNode.classList.contains('GridProducts')){
-    console.log(document.getElementsByClassName('products')[index+3].href);;
-    window.location.href = document.getElementsByClassName('products')[index+3].href;
-    // document.getElementsByClassName('products')[index].href = document.getElementsByClassName('products')[index+2].href;
-    // let box = document.getElementsByClassName('products')[index+2].href;)
- }
-//  console.log(document.getElementsByClassName('products')[index+2].href);
-})
+let resultRightBox = document.getElementsByClassName('ResultsRigthBoxWrapper')[0];
+resultRightBox.addEventListener('click', function(event) {
+  'use strict';
+  let products = document.getElementsByClassName('productListThumb');
+  let index = [...products].findIndex(el => el === event.target);
+  if (event.target.classList.contains('productListThumb')) {
+    window.location.href = document.getElementsByClassName('productListThumb')[index+3].href;
+  }
+});
