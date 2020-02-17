93098891e57e9eb6f07b10f7105a36d59f4323d8
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-02-17 21:41:09 +0600

    УТВ

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 let resultRightBox = document.getElementsByClassName('ResultsRigthBoxWrapper')[0];
 resultRightBox.addEventListener('click', function(event) {
   'use strict';
+  event.preventDefault();
   let products = document.getElementsByClassName('productListThumb');
   let index = [...products].findIndex(el => el === event.target);
   if (event.target.classList.contains('productListThumb')) {
     window.location.href = document.getElementsByClassName('productListThumb')[index+3].href;
+    console.log(window.location.href = document.getElementsByClassName('productListThumb')[index+3].href)
   }
 });
